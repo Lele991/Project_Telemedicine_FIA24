ed4bd859911187ff72d2406c6b7c189d51fb5588
Gabriele Peluzzi <g.peluzzi@teleconsys.it>
2024-09-03 23:48:57 +0200

    allineato managedata con fix delle classi di preprocessing e della nuova classa feature extractor

---
+++ dataprocessing/FeatureExtractor.py
@@ -0,0 +1,156 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class FeatureExtractor:
+    def __init__(self, dataset):
+        """
+        Inizializza la classe con il dataset già pulito e normalizzato.
+        """
+        self.dataset = dataset.copy()
+        self.incremento_percentuale_medio = None
+
+    def get_dataset(self):
+        """
+        Ritorna il dataset attuale.
+        """
+        return self.dataset
+
+    def calculate_percentage_increments(self):
+        """Calcola gli incrementi percentuali di una colonna specificata e li aggiunge al dataset."""
+        # Definizione delle colonne per il raggruppamento
+        cols_grouped = ['anno', 'trimestre', 'codice_descrizione_attivita']
+        
+        # Raggruppamento dei dati e conteggio dei servizi
+        df_grouped = self.dataset.groupby(cols_grouped).size().reset_index(name='numero_servizi')
+        
+        # Rimozione della colonna 'anno' per calcolare l'incremento per ciascun gruppo
+        df_cols_no_anno = cols_grouped.copy()
+        df_cols_no_anno.remove('anno')
+        
+        # Calcolo dell'incremento del numero di servizi per ciascun gruppo
+        df_grouped['incremento'] = df_grouped.groupby(df_cols_no_anno)['numero_servizi'].diff()
+        
+        # Calcolo dell'incremento percentuale
+        df_grouped['incremento_percentuale'] = df_grouped['incremento'] / df_grouped.groupby(df_cols_no_anno)['numero_servizi'].shift(1) * 100
+        #df_grouped['incremento_percentuale'].fillna(0, inplace=True)
+
+        self.incremento_percentuale_medio = df_grouped.groupby(df_cols_no_anno)['incremento_percentuale'].mean().reset_index()
+        
+        # Unisce i risultati al dataset originale
+        #self.dataset = pd.merge(self.dataset, df_grouped[['anno', 'trimestre', 'codice_descrizione_attivita', 'fascia_eta', 'incremento', 'incremento_percentuale']],
+        #                        on=['anno', 'trimestre', 'codice_descrizione_attivita', 'fascia_eta'], how='left')
+
+    def add_quarter_column(self):
+        """
+        Aggiunge una colonna che indica il trimestre di erogazione del servizio,
+        gestendo correttamente le differenze di fuso orario.
+        """
+        # Assicuriamoci che la colonna 'data_erogazione' sia in formato datetime e UTC
+        self.dataset['data_erogazione'] = pd.to_datetime(self.dataset['data_erogazione'], utc=True)
+        
+        # Rimuoviamo il fuso orario per evitare l'avviso
+        self.dataset['data_erogazione'] = self.dataset['data_erogazione'].dt.tz_localize(None)
+        
+        # Creiamo una nuova colonna con il trimestre di erogazione del servizio
+        self.dataset['trimestre'] = self.dataset['data_erogazione'].dt.to_period('Q')
+
+    def preprocess_data(self):
+        """
+        Preprocessa i dati per l'analisi, convertendo le date e creando nuove colonne per anno e trimestre.
+        """
+        # Conversione della colonna 'data_erogazione' in datetime e gestione dei fusi orari
+        self.dataset['data_erogazione'] = pd.to_datetime(self.dataset['data_erogazione'], utc=True, errors='coerce')
+        
+        if self.dataset['data_erogazione'].isnull().any():
+            print("Warning: Ci sono valori mancanti in 'data_erogazione'.")
+        
+        # Creazione delle colonne 'anno' e 'trimestre'
+        self.dataset['anno'] = self.dataset['data_erogazione'].dt.year
+        self.dataset['trimestre'] = ((self.dataset['data_erogazione'].dt.month - 1) // 3) + 1
+
+    def compute_numero_servizi(self):
+        """
+        Calcola il numero di servizi per anno, trimestre e codice_descrizione_attivita.
+        """
+        # Raggruppamento per anno, trimestre e codice_descrizione_attivita
+        cols_grouped = ['anno', 'trimestre', 'codice_descrizione_attivita']
+        df_grouped = self.dataset.groupby(cols_grouped).size().reset_index(name='numero_servizi')
+        self.dataset = pd.merge(self.dataset, df_grouped, on=cols_grouped, how='left')
+        return df_grouped
+
+    def plot_graph(self):
+        """
+        Crea e salva i grafici della distribuzione degli incrementi e delle categorie di incremento,
+        e l'andamento trimestrale delle teleassistenze.
+        """
+        output_dir = 'graphs'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+    
+        incremento_data = self.incremento_percentuale_medio['incremento_percentuale']
+        column = 'attività'
+
+        # Creazione della figura combinata
+        plt.figure(figsize=(18, 12))
+
+        # Distribuzione degli incrementi percentuali
+        plt.subplot(2, 2, 1)
+        sns.histplot(incremento_data, bins=30, kde=True, color='skyblue')
+        plt.title(f'Distribuzione degli Incrementi Percentuali ({column})')
+        plt.xlabel('Variazione Percentuale Media dei Servizi (%)')
+        plt.ylabel('Numero di Occorrenze') # indica il numero di volte che un determinato incremento percentuale medio si verifica nel dataset
+        plt.grid(True)
+
+        # Boxplot degli incrementi percentuali
+        plt.subplot(2, 2, 2)
+        sns.boxplot(x=incremento_data, color='salmon')
+        plt.title(f'Boxplot degli Incrementi Percentuali ({column})')
+        plt.xlabel('Variazione Percentuale dei Servizi (%)')
+        plt.grid(True)
+
+        # Grafico dell'andamento trimestrale delle teleassistenze
+        plt.subplot(2, 1, 2)
+        trend = self.dataset.groupby('trimestre').size()
+        # Generazione delle etichette personalizzate
+        labels = []
+        previous_year = None
+        for period in trend.index:
+            year = period.year
+            quarter = period.quarter
+            if quarter == 1:
+                labels.append(f"{year}")
+                previous_year = year
+            else:
+                labels.append(f"Q{quarter}")
+
+        # Plot del trend con le etichette personalizzate
+        plt.plot(trend.index.astype(str), trend.values, marker='o', linestyle='-')
+        plt.xticks(ticks=range(len(trend.index)), labels=labels, rotation=45)
+        plt.title('Andamento delle Teleassistenze per Trimestre')
+        plt.xlabel('Periodo (Anno e Trimestre)')
+        plt.ylabel('Numero di Teleassistenze')
+        plt.grid(True)
+
+        # Salvataggio della figura combinata
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, 'combined_plot.png'))
+        plt.close()
+
+    def run_analysis(self):
+        """
+        Esegue l'intera pipeline di analisi: preprocessa i dati, calcola gli incrementi percentuali,
+        aggiunge la colonna del trimestre, e genera i grafici richiesti.
+        """
+        # Preprocessing dei dati
+        self.preprocess_data()
+        
+        # Calcolo degli incrementi percentuali
+        self.calculate_percentage_increments()
+        
+        # Aggiunta della colonna del trimestre
+        self.add_quarter_column()
+        
+        # Generazione e salvataggio dei grafici
+        self.plot_graph()
--- dataprocessing/ManageData.py
+++ dataprocessing/ManageData.py
@@ -1,13 +1,13 @@
-# classe principale per la gestione dei dati
-# -->
-# chaimata al file DataFix per popolare i campi mancanti
-# chiamata al file DataCleaner per la pulizia dei dati
-
-
 import numpy as np
 import pandas as pd
+import logging
 
+from dataprocessing.FeatureSelection import FeatureSelection
+from dataprocessing.FeatureExtractor import FeatureExtractor
 from dataprocessing.manage import DataCleaner, DataFix
+
+# Configurazione del logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class ManageData:
     def __init__(self, dataset, path_province, path_comuni, missing_threshold=0.6):
@@ -38,19 +38,12 @@ class ManageData:
         
         # Sostituisce 'None' e None con NaN
         self.dataset.replace({'None': np.nan, None: np.nan}, inplace=True)
+        logging.info("Sostituiti i valori 'None' e None con NaN nel dataset.")
 
     def clean_data(self):
         """Esegue una pulizia completa dei dati."""
         df = self.dataset
-        #columns = [
-        # data inpiut 1/2:
-        #    'id_prenotazione', 'id_paziente', 'codice_regione_residenza', 'codice_asl_residenza', 'codice_provincia_residenza',
-        #    'codice_comune_residenza', 'descrizione_attivita', 'data_contatto', 'data_disdetta',
-        # data inpiut 2/2:
-        #    'codice_regione_erogazione', 'struttura_erogazione', 'tipologia_struttura_erogazione', 'id_professionista_sanitario',
-        #    'tipologia_professionista_sanitario', 'codice_tipologia_professionista_sanitario', 'ora_inizio_erogazione', 'ora_fine_erogazione', 'data_erogazione'
-        #]
-
+        
         # Rimuove duplicati
         df = DataCleaner.remove_duplicates(df)
 
@@ -60,15 +53,45 @@ class ManageData:
         # Fix province e comuni
         df = DataFix.fill_province_comuni(df, self.path_province, self.path_comuni)
 
+        # Rimuove colonne con valori mancanti sopra la soglia
         df = DataCleaner.remove_missing_values_rows(df, self.missing_threshold)
         
+        # Aggiunge durata della visita e riempie le durate mancanti
         df = DataFix.add_durata_visita(df)
         df = DataFix.fill_durata_visita(df)
+        
+        # Aggiunge l'età del paziente
         df = DataFix.add_eta_paziente(df)
 
+        # Aggiunge la fascia d'età
+        df = DataFix.add_fascia_eta_column(df)
+
+        # Gestisce i valori mancanti nel dataset, riempiendo i valori mancanti in base alla media
+        #df = DataCleaner.handle_missing_values(df)
+
+        # Identifica e gestisce gli outlier
         df = DataCleaner.update_dataset_with_outliers(df)
 
-        columns = ['ora_inizio_erogazione', 'ora_fine_erogazione']
+        # Rimuove colonne non più necessarie
+        columns = ['ora_inizio_erogazione', 'ora_fine_erogazione',
+                   'id_prenotazione', 'id_paziente', 'regione_residenza', 'codice_regione_residenza',
+                   'asl_residenza', 'codice_asl_residenza', 'provincia_residenza',
+                   'comune_residenza', 'codice_comune_residenza', 'descrizione_attivita',
+                   'data_contatto', 'regione_erogazione', 'asl_erogazione', 'provincia_erogazione',
+                   'tipologia_struttura_erogazione', 'id_professionista_sanitario',
+                   'tipologia_professionista_sanitario', 'codice_tipologia_professionista_sanitario']
+
         df = DataCleaner.remove_columns(df, columns)
 
+        # Imposta il dataset pulito
         self.set_dataset(df)
+        logging.info("Pulizia completa del dataset eseguita con successo.")
+
+        featureExtractor = FeatureExtractor(self.dataset)
+        featureExtractor.run_analysis()
+        df = featureExtractor.get_dataset()
+        self.set_dataset(df)
+
+        featureSelection = FeatureSelection(df)
+        featureSelection.execute_feature_selection()
+

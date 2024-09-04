5f6f22aa9f0d436ec2ebd57d98463169199ba04b
Gabriele Peluzzi <g.peluzzi@teleconsys.it>
2024-09-04 14:06:45 +0200

    gli outliers ora vengono rimossi dal dataset

--- dataprocessing/ManageData.py
+++ dataprocessing/ManageData.py
@@ -100,5 +100,5 @@ class ManageData:
         self.set_dataset(df)
 
         featureSelection = FeatureSelection(df)
-        featureSelection.execute_feature_selection(remove_others_colum_by_threshold=False)
+        featureSelection.execute_feature_selection(threshold=0.85,remove_others_colum_by_threshold=True)
 
